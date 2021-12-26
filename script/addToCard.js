33c33ea56cd39decb7674573320f8249af9e9f71
Exem <chernyakov.ghost@mail.ru>
2021-12-26 16:07:19 +0500

    выфа

--- script/addToCard.js
+++ script/addToCard.js
@@ -1,13 +1,17 @@
 var data;
 
-$(".addToCard").on("click", function () {
-  var parent_id = $(this).parent();
-  parent_id.each(function () {
-    var $this = $(this);
-    data = {
-      heading: $this.find(".offer-heading").text(),
-      text: $this.find(".offer-text").text(),
-    };
-  });
-  localStorage.setItem("currentOffer", JSON.stringify(data));
+$(document).ready(function () {
+  setTimeout(() => {
+    $(".addToCard").on("click", function () {
+      var parent_id = $(this).parent();
+      parent_id.each(function () {
+        var $this = $(this);
+        data = {
+          heading: $this.find(".offer-heading").text(),
+          text: $this.find(".offer-text").text(),
+        };
+      });
+      localStorage.setItem("currentOffer", JSON.stringify(data));
+    });
+  }, 500);
 });
--- script/formValidate.js
+++ script/formValidate.js
@@ -1,8 +1,27 @@
 $("#formContact").submit(function (event) {
-  if($('#name').text() == '') alert('Пожалуйста, введите ваше ФИО')
-  if($('#email').text() == '') alert('Пожалуйста, введите ваш email')
-  if($('#phone').text() == '') alert('Пожалуйста, введите ваш номер телефона')
-  if($('#problem-text').text() == '') alert('Пожалуйста, опишите ваши пожелания')
-
   event.preventDefault();
+  let valid = true;
+  if (document.querySelector("#name").value == "") {
+    valid = false;
+    alert("Пожалуйста, введите ваше ФИО");
+  }
+  if (document.querySelector("#email").value == "") {
+    valid = false;
+    alert("Пожалуйста, введите ваш email");
+  }
+  if (document.querySelector("#phone").value == "") {
+    valid = false;
+    alert("Пожалуйста, введите ваш номер телефона");
+  }
+  if (document.querySelector("#problem-text").value == "") {
+    valid = false;
+    alert("Пожалуйста, опишите ваши пожелания");
+  }
+  if (valid) {
+    alert("Отправлено! С вами обязательно свяжутся в ближайшее когда нибудь");
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#phone").value = "";
+    document.querySelector("#problem-text").value = "";
+  }
 });
